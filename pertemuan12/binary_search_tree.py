33e9901a7949ddc61d7450679bdd130d1611c9c8
dafaislamy <dafaislamy1607@gmail.com>
2026-05-03 22:01:51 +0700

    initial commit

--- pertemuan12/binary_search_tree.py
+++ pertemuan12/binary_search_tree.py
@@ -1,76 +1,76 @@
-class Node:
+class Node: #deklarasi class Node dengan isi id_buku, judul, left, dan right
     def __init__(self, id_buku, judul):
         self.id_buku = id_buku
         self.judul = judul
         self.left = None
         self.right = None
 
-class BinarySearchTree:
+class BinarySearchTree: #deklarasi class BinarySearchTree
     def __init__(self):
         self.root = None
 
-    def insert(self, id_buku, judul):
-        new_node = Node(id_buku, judul)
-        if self.root is None:
-            self.root = new_node
+    def insert(self, id_buku, judul): #deklarasi fungsi insert untuk memasukkan data kedalam tree dengan syarat jika data lebih kecil dari root, maka diletak disebelah kiri root, jika data lebih besar dari root, maka diletak disebelah kenan root
+        new_node = Node(id_buku, judul) #deklarasi variabel new_node untuk membuat sebuah node baru
+        if self.root is None: #jika root kosong, maka baris dibawah tidak dijalankan
+            self.root = new_node #jika root berisi, maka root diisi new_node
             print(f"[INSERT] Berhasil memasukkan: ID {id_buku} - {judul}")
             return
         
-        current = self.root
+        current = self.root #deklarasi variabel current dan diisi root
         while True:
-            if id_buku < current.id_buku:
-                if current.left is None:
-                    current.left = new_node
+            if id_buku < current.id_buku: #jika id_buku lebih kecil dari id_buku saat ini, maka baris kode dijalankan
+                if current.left is None: #jika current.left = None atau masih kosong, maka
+                    current.left = new_node #current.left diisi new_node
                     break
-                current = current.left
+                current = current.left #jika current.left sudah ada isinya, maka current = current.left
 
-            if id_buku > current.id_buku:
-                if current.right is None:
-                    current.right = new_node
+            if id_buku > current.id_buku: #jika id_buku lebih besar dari id_buku saat ini, maka baris kode dijalankan
+                if current.right is None: #jika current.right = None atau masih kosong, maka
+                    current.right = new_node #current.right diisi new_node
                     break
-                current = current.right
+                current = current.right #jika current.right sudah ada isinya, maka current = current.right
 
         print(f"[INSERT] Berhasil memasukkan: ID {id_buku} - {judul}")
 
-    def search(self, id_buku):
-        current = self.root
-        while current is not None:
-            if id_buku == current.id_buku:
-                return current
-            elif id_buku < current.id_buku:
-                current = current.left
-            else:
-                current = current.right
-        return None
+    def search(self, id_buku): #deklarasi fungsi search untuk mencari suatu data didalam tree
+        current = self.root #deklarasi variabel current dan diisi root
+        while current is not None: #selama current tidak kosong, jalankan baris kode 
+            if id_buku == current.id_buku: #jika id_buku sama dengan id_buku saat ini
+                return current #maka kembalikan current
+            elif id_buku < current.id_buku: #jika id_buku lebih kecil dari id_buku saat ini
+                current = current.left #maka current = current.left
+            else: #jika tidak memenuhi keduanya yaitu id_buku lebih besar dari id_buku saat ini
+                current = current.right #maka current = current.right
+        return None #jika current = None atau kosong, kembalikan None
 
-    def traversal_inorder(self, node):
+    def traversal_inorder(self, node): #deklarasi fungsi traversal_inorder untuk mengurutkan data mulai dari left - root - right yang mana akan menampilkan data yang tersusun dari terkecil ke terbesar
         if node:
             self.traversal_inorder(node.left)
             print(f"{node.id_buku} - {node.judul}")
             self.traversal_inorder(node.right)
 
-    def get_min(self):
-        if self.root is None:
-            return None
-        current = self.root
-        while current.left:
-            current = current.left
-        return current.id_buku
+    def get_min(self): #deklarasi fungsi get_min untuk mencari id_buku terkecil dari tree
+        if self.root is None: #jika root masih kosong
+            return None #maka kembalikan None
+        current = self.root #deklarasi variabel current dan diisi root
+        while current.left: #selama masih ada current.left
+            current = current.left #maka current diisi current.left
+        return current.id_buku #jika sudah sampai pada ujung tree atau leaf, maka sudah sampai pada tujuan dimana data merupakan data terkecil didalam tree, maka kembalikan id_buku nya
 
-    def get_max(self):
-        if self.root is None:
-            return None
-        current = self.root
-        while current.right:
-            current = current.right
-        return current.id_buku
+    def get_max(self): #deklarasi fungsi get_max untuk mencari id_buku terbesar dari tree
+        if self.root is None: #jika root masih kosong
+            return None #maka kembalikan None
+        current = self.root #deklarasi variabel current dan diisi root
+        while current.right: #selama masih ada current.right
+            current = current.right #maka current diisi current.right
+        return current.id_buku #jika sudah sampai pada ujung tree atau leaf, maka sudah sampai pada tujuan dimana data merupakan data terbesar didalam tree, maka kembalikan id_buku nya
 
-    def height(self, node):
-        if node is None:
-            return -1
-        left_height = self.height(node.left)
-        right_height = self.height(node.right)
-        return max(left_height, right_height) + 1
+    def height(self, node): #deklarasi fungsi height untuk menghitung tinggi (level) dari tree
+        if node is None: #jika node masih kosong
+            return -1 #kembalikan -1, karena tree masih kosong sehingga belum ada level
+        left_height = self.height(node.left) #left_height melakukan rekursi dengan memanggil fungsinya kembali yang berfungi untuk sampai pada ujung tree atau leaf disebelah kiri
+        right_height = self.height(node.right) #right_height melakukan rekursi dengan memanggil fungsinya kembali yang berfungsi untuk sampai pada ujung tree atau leaf disebelah kanan
+        return max(left_height, right_height) + 1 #fungsi mengembalikan ukuran terbesar (max) dari left_height dan right_height kemudian ditambah 1 agar perhitungan tetap bertambah
 
 
 print('SISTEM KATALOG PERPUSTAKAAN "ILMU TERANG"')
--- pertemuan12/binary_tree.py
+++ pertemuan12/binary_tree.py
@@ -1,44 +1,44 @@
-class Node:
+class Node: #deklarasi class Node dengan isi data, left dan right
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-class BinaryTree:
+class BinaryTree: #deklarasi class BinaryTree
     def __init__(self):
         self.root = None
 
-    def insert_manual(self):
-        self.root = Node("A")
+    def insert_manual(self): #deklarasi fungsi insert_manual untuk memasukkan data satu per satu kedalam tree
+        self.root = Node("A") #Node A menjadi root dalam tree (level 0)
 
-        self.root.left = Node("B")
-        self.root.right = Node("C")
+        self.root.left = Node("B") #Node B terletak di sebelah kiri root (level 1)
+        self.root.right = Node("C") #Node C terletak di sebelah kanan root (level 1)
 
-        self.root.left.left = Node("D")
-        self.root.left.right = Node("E")
+        self.root.left.left = Node("D") #Node D terletak di sebelah kiri Node B (level 2)
+        self.root.left.right = Node("E") #Node E terletak di sebelah kanan Node B (level 2)
 
-        self.root.right.right = Node("F")
+        self.root.right.right = Node("F") #Node F terletak di sebelah kanan Node C (level 2), bagian kiri Node C kosong
 
 
-def traverse_preorder(node):
+def traverse_preorder(node): #preorder akan mengunjungi tree mulai dari root - left - right
     if node is not None:
         print(node.data, end=" ")
         traverse_preorder(node.left)
         traverse_preorder(node.right)
 
-def traverse_inorder(node):
+def traverse_inorder(node): #inorder akan mengunjungi tree mulai dari left - root - right
     if node is not None:
         traverse_inorder(node.left)
         print(node.data, end=" ")
         traverse_inorder(node.right)
 
-def traverse_postorder(node):
+def traverse_postorder(node): #postorder akan mengunjungi tree mulai dari left - right - root
     if node is not None:
         traverse_postorder(node.left)
         traverse_postorder(node.right)
         print(node.data, end=" ")
 
-def get_leaf_nodes(node):
+def get_leaf_nodes(node): #fungsi ini berguna untuk menampilkan node yang merupakan leaf (node yang tidak memiliki anak) didalam tree
     if node:
         if not node.left and not node.right:
             print(node.data, end=" ")
